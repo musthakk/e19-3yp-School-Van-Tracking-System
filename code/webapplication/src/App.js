00e5fb0e499c381de20e4babd07d4b70f4e1af25
Vidura_Yashan <yashanwagaarachchi@gmail.com>
2024-01-07 00:59:38 +0530

    UserInterface

--- code/webapplication/src/App.js
+++ code/webapplication/src/App.js
@@ -1,12 +1,38 @@
-import "./App.css";
-import Login from "./login";
+import React, { Component } from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
+import NavBar from "./components/navBar";
+import Track from "./components/Track";
+import Bus from "./components/Bus";
+import RegisterUser from "./components/RegisterUser";
+import UserRecord from "./components/UserRecord";
+import DriverRecord from "./components/DriverRecord";
+import ExtraService from "./components/ExtraService";
+import Home from "./components/home";
+import NotFound from "./components/notFound";
 
-function App() {
-  return (
-    <>
-      <Login />
-    </>
-  );
+class App extends Component {
+  state = {};
+  render() {
+    return (
+      <>
+        <NavBar />
+        <main className="container">
+          <Switch>
+            <Route path="/Track" component={Track} />
+            <Route path="/Bus" component={Bus} />
+            <Route path="/RegisterUser" component={RegisterUser} />
+            <Route path="/UserRecord" component={UserRecord} />
+            <Route path="/DriverRecord" component={DriverRecord} />
+            <Route path="/ExtraService" component={ExtraService} />
+            <Route path="/not-found" component={NotFound} />
+            <Route path="/home" component={Home} />
+            <Redirect from="/" exact to="/home" />
+            <Redirect to="/not-found" />
+          </Switch>
+        </main>
+      </>
+    );
+  }
 }
 
 export default App;
---
+++ code/webapplication/src/components/Track.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class Track extends Component {
+  state = {};
+  render() {
+    return <h1>Track</h1>;
+  }
+}
+
+export default Track;

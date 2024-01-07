aa6bc993f81aff796aaea0280ba395ab53ebf202
Vidura_Yashan <yashanwagaarachchi@gmail.com>
2024-01-07 16:48:08 +0530

    Error resolved

--- code/webapplication/src/components/Bus.jsx
+++ code/webapplication/src/components/Bus.jsx
@@ -8,13 +8,31 @@ class Bus extends Form {
     data: {
       licencePlate: "",
       school: "",
+      sheets: "",
     },
     errors: {},
+    busses: [
+      {
+        licencePlate: "AA-1234",
+        school: "St. Mary's",
+        sheets: 10,
+        assignedChildren: 10,
+        assignedDriver: "John Doe",
+      },
+      {
+        licencePlate: "AB-1234",
+        school: "St. Mary's",
+        sheets: 10,
+        assignedChildren: 10,
+        assignedDriver: "John Doe",
+      },
+    ],
   };
 
   schema = {
     licencePlate: Joi.string().required().label("Licence Plate"),
     school: Joi.string().required().label("School"),
+    sheets: Joi.number().required().label("Sheets"),
   };
 
   doSubmit = async () => {
@@ -23,6 +41,9 @@ class Bus extends Form {
       await addNewBus({
         licencePlateNumber: data.licencePlate,
         school: data.school,
+        sheets: data.sheets,
+        SheetsFilled: 0,
+        Driver: "",
       });
       // Optionally, you can redirect the user or perform other actions after the bus is successfully added.
       console.log("Bus added successfully!");
@@ -31,7 +52,7 @@ class Bus extends Form {
     }
   };
 
-  render() {
+  render({ busses } = this.state) {
     return (
       <div className="row">
         <div className="col-4">
@@ -39,8 +60,33 @@ class Bus extends Form {
           <form onSubmit={this.handleSubmit}>
             {this.renderInput("licencePlate", "Licence Plate")}
             {this.renderInput("school", "School")}
+            {this.renderInput("sheets", "Sheets")} <br />
             {this.renderButton("Save")}
           </form>
+        </div>
+        <div className="col">
+          <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">Licence Plate</th>
+                <th scope="col">School</th>
+                <th scope="col">Sheets</th>
+                <th scope="col">Sheets filled</th>
+                <th scope="col">Driver</th>
+              </tr>
+            </thead>
+            <tbody>
+              {busses.map((bus) => (
+                <tr key={bus.licencePlate}>
+                  <td>{bus.licencePlate}</td>
+                  <td>{bus.school}</td>
+                  <td>{bus.sheets}</td>
+                  <td>{bus.assignedChildren}</td>
+                  <td>{bus.assignedDriver}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
       </div>
     );

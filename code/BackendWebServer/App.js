ab3d7d852a085fef7d207d0fc1df080f4067348c
i-am-shantosh <e19371@eng.pdn.ac.lk>
2024-01-29 09:25:56 +0530

    in backEnd App.js created don't use

---
+++ code/BackendWebServer/App.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const app = express();
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const port = 8000;
+const _dirname = path.dirname("");
+const buildPath = path.join(_dirname, "../webapplication/build");
+
+app.use(express.static(buildPath));
+
+app.get("/*", function (req, res) {
+  res.sendFile(
+    path.join(__dirname, "../webapplication/build/index.html"),
+    function (err) {
+      if (err) {
+        res.status(500).send(err);
+      }
+    }
+  );
+});
+
+app.get("/*", function (req, res) {
+  const indexPath = path.join(buildPath, "index.html");
+
+  res.sendFile(indexPath, function (err) {
+    if (err) {
+      res.status(500).send(err);
+    }
+  });
+});
+
+// ... rest of your routes and server setup
+
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});

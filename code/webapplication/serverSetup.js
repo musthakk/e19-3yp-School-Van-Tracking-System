8f45d9cd452842fe460bfe81415daeeca6f717a6
i-am-shantosh <e19371@eng.pdn.ac.lk>
2024-01-29 16:32:30 +0530

    config.json changed backend.js port changed

--- code/webapplication/serverSetup.js
+++ code/webapplication/serverSetup.js
@@ -7,7 +7,7 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const port = 3000;
+const port = 8000;
 const _dirname = path.dirname("");
 const buildPath = path.join(_dirname, "./build");
 app.use(express.static(buildPath));
@@ -19,18 +19,6 @@ app.get("/*", function (req, res) {
     }
   });
 });
-
-app.get("/*", function (req, res) {
-  const indexPath = path.join(buildPath, "index.html");
-
-  res.sendFile(indexPath, function (err) {
-    if (err) {
-      res.status(500).send(err);
-    }
-  });
-});
-
-// ... rest of your routes and server setup
 
 app.listen(port, "0.0.0.0", () => {
   console.log(`Server is running on http://localhost:${port}`);

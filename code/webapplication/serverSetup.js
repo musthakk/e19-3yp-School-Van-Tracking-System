7c872df92f86ed6e3e4af95adde0ea1de29ffe07
i-am-shantosh <e19371@eng.pdn.ac.lk>
2024-01-29 13:53:54 +0530

    files added

--- code/webapplication/serverSetup.js
+++ code/webapplication/serverSetup.js
@@ -9,19 +9,15 @@ app.use(express.json());
 
 const port = 3000;
 const _dirname = path.dirname("");
-const buildPath = path.join(_dirname, "../webapplication/build");
-
+const buildPath = path.join(_dirname, "./build");
 app.use(express.static(buildPath));
 
 app.get("/*", function (req, res) {
-  res.sendFile(
-    path.join(__dirname, "../webapplication/build/index.html"),
-    function (err) {
-      if (err) {
-        res.status(500).send(err);
-      }
+  res.sendFile(path.join(__dirname, "./build/index.html"), function (err) {
+    if (err) {
+      res.status(500).send(err);
     }
-  );
+  });
 });
 
 app.get("/*", function (req, res) {
